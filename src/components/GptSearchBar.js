8d5880c640f507a866abc667924ff02afd1cf419
niteshgiri-7 <niteshgiri1717@gmail.com>
2024-07-28 12:14:14 +0545

    multiLang done

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,11 +1,17 @@
 import React, { useRef } from "react";
 import { HOME_IMG_URL } from "../utils/constants";
+import lang from "../utils/langData";
+import { useSelector } from "react-redux";
 const GptSearchBar = () => {
   const searchText = useRef(null);
-
+  const langOpt = useSelector((store) => store.lang);
+  const langKey = langOpt.chosenLang;
   const handleSearch = () => {
     const searchedVal = searchText.current.value;
     console.log(searchedVal);
+    console.log("userchosen", langOpt);
+    const langKey = langOpt.chosenLang;
+    console.log(lang[langKey]);
   };
   return (
     <>
@@ -17,15 +23,17 @@ const GptSearchBar = () => {
 
       <div className="flex justify-center">
         <div className="w-6/12  h-2/4 fixed  flex justify-center items-center ">
-          <div className="w-[70%] h-[20%] align-middle bg-black">
+          <div className="w-[70%] h-[20%] align-middle bg-black bg-opacity-80 rounded-md">
             <input
               className="m-4 p-4 w-[80%] rounded-md "
-              placeholder="what do you like to watch today?"
+              placeholder={lang[langKey].gptSearchPlaceHolder}
               ref={searchText}
-              
             ></input>
-            <button className="px-6 py-4 bg-red-700 rounded-lg text-white font-bold" onClick={()=>handleSearch()}> 
-              Search
+            <button
+              className="px-6 py-4 bg-red-700 rounded-lg text-white font-bold"
+              onClick={() => handleSearch()}
+            >
+              {lang[langKey].search}
             </button>
           </div>
         </div>
---
+++ src/utils/langData.js
@@ -0,0 +1,18 @@
+
+const lang = {
+    en:{
+        search:"search",
+        gptSearchPlaceHolder:"what do you like to watch today?",
+    },
+    nep:{
+        search:"खोज",
+        gptSearchPlaceHolder:"तपाई आज के हेर्न चाहनुहुन्छ?",
+    },
+    esp:{
+        search:"buscar",
+        gptSearchPlaceHolder:"¿Qué te gusta ver hoy?",
+    }
+}
+
+
+export default lang;
