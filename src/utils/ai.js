9503f04894017d6224bed809f130b0cc8515aa97
niteshgiri-7 <niteshgiri1717@gmail.com>
2024-07-28 13:13:47 +0545

    key in env

--- src/utils/ai.js
+++ src/utils/ai.js
@@ -1,7 +1,7 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI("AIzaSyDRRSIPvWPj4FZklWmsGc-OExXkAV6GoP4");
+const genAI = new GoogleGenerativeAI(process.env.REACT_APP_AI_KEY);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
